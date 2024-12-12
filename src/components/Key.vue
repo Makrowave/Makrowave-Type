8274bd68de7ecfe8dc2da5dcbcc895af132a0367
makrowave <maksstyzej@gmail.com>
2024-12-12 19:27:23 +0100

    feat: not working key :)

---
+++ src/components/Key.vue
@@ -0,0 +1,41 @@
+<script>
+
+const props = defineProps({
+    type: string,
+    key: string
+})
+
+const isPressed = ref(false);
+const pressedClass = ref('pressed')
+const normalClass = ref('normal')
+const handleKeyDown = (event) => {
+    if(event.key === key) {
+        isPressed.value = true;
+    }
+}
+
+const handleKeyUp = (event) => {
+    if(event.key === key) {
+        isPressed.value = false;
+    }
+}
+</script>
+
+<template>
+<div @keydown="handleKeyDown" @keyup="handleKeyUp" :class="isPressed ? pressedClass : normalClass">
+    <slot>Key</slot>
+</div>
+</template>
+
+<style>
+.normal {
+    background-color: aquamarine;
+    height: 40px;
+    width: 40px;
+}
+.pressed {
+    background-color: cadetblue;
+    height: 40px;
+    width: 40px;
+}
+</style>
