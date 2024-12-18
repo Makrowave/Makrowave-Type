ed9d0f9c76fadb60b814a13aceb81896abdd5d62
EegleZ <maksstyzej@gmail.com>
2024-12-18 19:26:01 +0100

    feat: settings

--- src/routes/SettingsPage.vue
+++ src/routes/SettingsPage.vue
@@ -6,16 +6,76 @@ import { onMounted, ref } from 'vue'
 const theme = useThemeStore()
 </script>
 <template>
-  <div class="option">
-    <ModalColorPicker v-model="theme.uiTextColor" />
+  <div class="settings">
+    <h3>UI</h3>
+    <section>
+      <div class="option">
+        <span>UI Text</span>
+        <ModalColorPicker v-model="theme.uiTextColor" />
+      </div>
+      <div class="option">
+        <span>UI Background</span>
+        <ModalColorPicker v-model="theme.uiBackground" />
+      </div>
+    </section>
+    <h3>Text</h3>
+    <section>
+      <div class="option">
+        <span>Incomplete</span>
+        <ModalColorPicker v-model="theme.inactiveFontColor" />
+      </div>
+      <div class="option">
+        <span>Incorrect</span>
+        <ModalColorPicker v-model="theme.incorrectFontColor" />
+      </div>
+      <div class="option">
+        <span>Correct</span>
+        <ModalColorPicker v-model="theme.correctFontColor" />
+      </div>
+    </section>
+    <h3>Keyboard</h3>
+    <section>
+      <div class="option">
+        <span>Inactive Key</span>
+        <ModalColorPicker v-model="theme.inactiveKeyColor" />
+      </div>
+      <div class="option">
+        <span>Inactive Key Text</span>
+        <ModalColorPicker v-model="theme.inactiveKeyTextColor" />
+      </div>
+      <div class="option">
+        <span>Active Key Text</span>
+        <ModalColorPicker v-model="theme.activeKeyTextColor" />
+      </div>
+      <div class="option">
+        <span>Moving Gradient</span>
+        <ModalColorPicker v-model="theme.outlineGradient" />
+      </div>
+    </section>
   </div>
 </template>
 <style>
+.settings {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 .color-square {
   height: 20px;
   width: 20px;
 }
 .option {
+  width: 100%;
   position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+section {
+  width: 200px;
+}
+h3 {
+  margin-bottom: 20px;
+  margin-top: 20px;
 }
 </style>
