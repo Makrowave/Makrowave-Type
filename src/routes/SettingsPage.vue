b026fe09745ea859e6d9b0c768c9f9fe7881da47
makrowave <maksstyzej@gmail.com>
2024-12-18 23:47:34 +0100

    refactor: settings changes

--- src/routes/SettingsPage.vue
+++ src/routes/SettingsPage.vue
@@ -1,14 +1,16 @@
 <script setup lang="ts">
 import ModalColorPicker from '@/components/colorpicker/ModalColorPicker.vue'
 import { useThemeStore } from '@/stores/theme'
-import { onMounted, ref } from 'vue'
+import { computed } from 'vue'
 
 const theme = useThemeStore()
+
+const shadow = computed(() => ({ boxShadow: `5px 5px 0px 7px ${theme.uiTextColor}` }))
 </script>
 <template>
   <div class="settings">
-    <h3>UI</h3>
-    <section>
+    <section :style="shadow">
+      <h3>UI</h3>
       <div class="option">
         <span>UI Text</span>
         <ModalColorPicker v-model="theme.uiTextColor" />
@@ -18,8 +20,9 @@ const theme = useThemeStore()
         <ModalColorPicker v-model="theme.uiBackground" />
       </div>
     </section>
-    <h3>Text</h3>
-    <section>
+
+    <section :style="shadow">
+      <h3>Text</h3>
       <div class="option">
         <span>Incomplete</span>
         <ModalColorPicker v-model="theme.inactiveFontColor" />
@@ -33,8 +36,9 @@ const theme = useThemeStore()
         <ModalColorPicker v-model="theme.correctFontColor" />
       </div>
     </section>
-    <h3>Keyboard</h3>
-    <section>
+
+    <section :style="shadow">
+      <h3>Keyboard</h3>
       <div class="option">
         <span>Inactive Key</span>
         <ModalColorPicker v-model="theme.inactiveKeyColor" />
@@ -52,24 +56,17 @@ const theme = useThemeStore()
         <ModalColorPicker v-model="theme.outlineGradient" />
       </div>
     </section>
-    <h3>Keyboard Gradient</h3>
-    <section>
+    <section :style="shadow">
+      <h3>Keyboard Gradient</h3>
       <div class="option" v-for="(color, i) in theme.activeKeyColors">
-        <button
-          class="themed-button"
-          :style="{ border: `0.5px solid ${theme.uiTextColor}` }"
-          v-if="i > 1"
-          @click="theme.activeKeyColors.splice(i, 1)"
-        >
+        <button class="themed-button" :style="{ border: `0.5px solid ${theme.uiTextColor}` }" v-if="i > 1"
+          @click="theme.activeKeyColors.splice(i, 1)">
           X
         </button>
         <ModalColorPicker v-model="theme.activeKeyColors[i]" />
       </div>
-      <button
-        @click="theme.activeKeyColors.push('#000000')"
-        class="themed-button"
-        :style="{ border: `0.5px solid ${theme.uiTextColor}`, width: '100%', marginTop: '6px' }"
-      >
+      <button @click="theme.activeKeyColors.push('#000000')" class="themed-button"
+        :style="{ border: `0.5px solid ${theme.uiTextColor}`, width: '100%', marginTop: '6px' }">
         +
       </button>
     </section>
@@ -85,15 +82,25 @@ const theme = useThemeStore()
   align-items: center;
   justify-content: center;
 }
-.settings {
+
+/*.settings {
   display: flex;
   flex-direction: column;
   align-items: center;
+}*/
+
+.settings {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 100px;
+  padding-top: 60px;
 }
+
 .color-square {
   height: 20px;
   width: 20px;
 }
+
 .option {
   margin: 1px 0 1px;
   width: 100%;
@@ -101,15 +108,20 @@ const theme = useThemeStore()
   align-items: center;
   justify-content: space-between;
 }
+
 .option div {
   margin-left: auto;
 }
+
 section {
   display: flex;
   align-items: center;
   flex-direction: column;
   width: 200px;
+  padding: 0 20px 20px 20px;
+
 }
+
 h3 {
   margin-bottom: 20px;
   margin-top: 20px;
