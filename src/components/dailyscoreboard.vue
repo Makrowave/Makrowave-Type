03f33635a176aa61ac580d5745e6fd4bdcbf819c
makrowave <maksstyzej@gmail.com>
2024-12-20 17:22:54 +0100

    feat: daily scoreboard

---
+++ src/components/dailyscoreboard.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { useThemeStore } from '@/stores/theme';
+
+const props = defineProps<{ scores: Array<{ id: number, username: string, time: number, accuracy: number }> }>()
+const theme = useThemeStore()
+</script>
+<template>
+  <div class="wrapper">
+    <h3>Daily</h3>
+    <table :style="{ border: `1px solid ${theme.uiTextColor}`, boxShadow: `10px 10px 0px 0px ${theme.uiTextColor}`, }">
+      <thead>
+        <tr :style="{ borderBottom: `1px solid ${theme.uiTextColor}` }">
+          <th>Username</th>
+          <th>Time</th>
+          <th>Accuracy</th>
+          <th>Score</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for=" score in props.scores" :key="score.id">
+          <td>{{ score.username }}</td>
+          <td>{{ score.time }}</td>
+          <td>{{ score.accuracy }}</td>
+          <td>{{ score.username }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+<style scoped>
+table {
+  border-collapse: collapse;
+}
+
+td,
+th {
+  padding: 0px 10px 0px 10px;
+  text-align: center;
+}
+
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
--- src/routes/LeaderboardPage.vue
+++ src/routes/LeaderboardPage.vue
@@ -1,3 +1,20 @@
-<script setup lang="ts"></script>
-<template></template>
-<style></style>
+<script setup lang="ts">
+import Scoreboard from '@/components/dailyscoreboard.vue';
+
+const scores = [
+  { id: 1, username: "Makrowave", time: 200000, accuracy: 0.9 },
+  { id: 2, username: "Makrowave", time: 200000, accuracy: 0.9 },
+  { id: 3, username: "Makrowave", time: 200000, accuracy: 0.9 },
+  { id: 4, username: "Makrowave", time: 200000, accuracy: 0.9 },
+  { id: 5, username: "Makrowave", time: 200000, accuracy: 0.9 }
+
+]
+</script>
+<template>
+  <Scoreboard class="margin" :scores="scores"></Scoreboard>
+</template>
+<style scoped>
+.margin {
+  margin-top: 20px;
+}
+</style>
